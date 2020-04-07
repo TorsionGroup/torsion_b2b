d8f2b4f596fae28a15bb6da7a53e7a59a4801a70
AlexGr33n <greentr33@gmail.com>
2020-04-07 10:00:12 +0300

    add Models Brand Migrate

--- torsion_shop/models.py
+++ torsion_shop/models.py
@@ -1,18 +1,19 @@
 from django.db import models
+from datetime import  date, datetime
 
 # Create your models here.
 
 class Brand(models.Model):
     name = models.CharField("Brand", max_length=250)
     enabled = models.IntegerField()
-    source_id = models.(max_length=250)
+    source_id = models.CharField(max_length=250)
     wait_list = models.IntegerField()
     is_recommended = models.IntegerField()
     sort_index = models.IntegerField()
-    source_type = models.(max_length=250)
-    gallery_attribute = models.(max_length=250)
-    gallery_name = models.(max_length=250)
-    kind = models.(max_length=250)
+    source_type = models.CharField(max_length=250)
+    gallery_attribute = models.CharField(max_length=250)
+    gallery_name = models.CharField(max_length=250)
+    kind = models.CharField(max_length=250)
     description = models.TextField("Description")
     url = models.SlugField(max_length=250, unique=True)
 
