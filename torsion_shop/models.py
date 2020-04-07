c11dfc00a632567b0b423f368ec3a21d2b72f1a2
AlexGr33n <greentr33@gmail.com>
2020-04-07 08:56:51 +0300

    add Models Brand

--- torsion_shop/models.py
+++ torsion_shop/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField("Brand", max_length=250)
+    enabled = models.IntegerField()
+    source_id = models.(max_length=250)
+    wait_list = models.IntegerField()
+    is_recommended = models.IntegerField()
+    sort_index = models.IntegerField()
+    source_type = models.(max_length=250)
+    gallery_attribute = models.(max_length=250)
+    gallery_name = models.(max_length=250)
+    kind = models.(max_length=250)
+    description = models.TextField("Description")
+    url = models.SlugField(max_length=250, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Brand"
+        verbose_name_plural = "Brands"
